f6e2e6816d4277ac4638274e07f0b297e99aea92
Oliver Haney <haney.oliver@gmail.com>
2023-06-25 00:43:14 -0400

    Initial commit

---
+++ lua/plugins/tmux-nav.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      {
+        "<C-h>",
+        "<cmd> TmuxNavigateLeft<CR>",
+        "window left",
+      },
+      {
+        "<C-j>",
+        "<cmd> TmuxNavigateDown<CR>",
+        "window down",
+      },
+      { "<C-k>", "<cmd> TmuxNavigateUp<CR>", "window up" },
+      { "C-l", "<cmd> TmuxNavigateRight<CR>", "window right" },
+    },
+  },
+}
